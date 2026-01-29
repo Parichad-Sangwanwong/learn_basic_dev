4bcc866ca0ec6db95c4940602c0712e28723469b
Parichad Sangwanwong <paricxx11@gmail.com>
2026-01-29 18:13:22 +0700

    Elysia

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,18 @@
 import { Elysia } from "elysia";
 import { openapi } from '@elysiajs/openapi'
 import 'dotenv/config';
-import { userController } from './modules/users/controller'
-import { postController } from './modules/posts/controller'
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+const db = drizzle(process.env.DATABASE_URL!);
 
 const app = new Elysia()
   .use(openapi())
   .get('/', 'Hello Elysia')
-  .use(userController)
-  .use(postController)
+  .get('/user/:id', ({ params: { id } }) => id)
+  .post('/form', ({ body }) => body)
   .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
